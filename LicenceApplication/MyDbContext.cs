126e96a51b5342ecc758709804cdd93cc3088fdd
PiotrB <piotr.baczkiewicz@regitech.pl>
2020-08-06 09:06:07 +0200

    AddDbContext

---
+++ LicenceApplication/MyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LicenceApplication
+{
+    class MyDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=LicenceDb;Trusted_Connection=True;");
+        }
+    }
+}
