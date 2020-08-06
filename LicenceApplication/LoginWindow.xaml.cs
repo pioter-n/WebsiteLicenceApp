affa5b7911a162e8941e58551e7ac6b7ca8d7931
PiotrB <piotr.baczkiewicz@regitech.pl>
2020-08-06 10:22:20 +0200

    Add events and main window

---
+++ LicenceApplication/LoginWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LicenceApplication
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+        private void BtnOK_Click(object sender, RoutedEventArgs e)
+        {
+            if (!String.IsNullOrEmpty(TBUser.Text) && !String.IsNullOrWhiteSpace(TBUser.Text))
+            {
+                MainWindow Main = new MainWindow();
+                Main.Show();
+                this.Close();
+            }
+        }
+
+        private void BtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
